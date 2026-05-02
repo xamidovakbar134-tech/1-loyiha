b9befd27c79a962ea478e0ff729b00ee9621e536
Akbar <xamidovakbar134@gmail.com>
2026-05-02 17:51:13 +0500

    section +

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
+import Section from "./_components/Section";
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero/>
+      <Section/>
     </div>
   );
 }
