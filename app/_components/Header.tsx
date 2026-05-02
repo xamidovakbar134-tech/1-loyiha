741b3eef99f2fa6f443a4d8fd037aef3b1c70a34
Akbar <xamidovakbar134@gmail.com>
2026-05-02 22:30:03 +0500

    header fixed

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,8 @@
 const Header = () => {
   return (
-    <div className="container mx-auto p-3">
-        <div className="flex items-center justify-center gap-220 bordeer-b-2 border-gray-300 border-b ">
+    <div className="fixed-top bg-white w-100 border-bottom">
+ <div className="container mx-auto p-3 shadow-sm">
+        <div className="flex items-center justify-center gap-220  border-gray-300 ">
             <h1 className="font-bold">Job Portal</h1>
             <div className="flex justify-center items-center gap-4">
                 <p className="mt-2 text-blue-400">Home</p>
@@ -10,6 +11,8 @@ const Header = () => {
             </div>
         </div>
     </div>
+    </div>
+   
   );
 };
 
