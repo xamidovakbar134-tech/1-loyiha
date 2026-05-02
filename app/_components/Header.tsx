5809a95c5c5548e89f41a2810a335d764d93f303
Akbar <xamidovakbar134@gmail.com>
2026-05-02 17:27:49 +0500

    header va hero qismi

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,16 @@
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto p-3">
+        <div className="flex items-center justify-center gap-220 bordeer-b-2 border-gray-300 border-b ">
+            <h1 className="font-bold">Job Portal</h1>
+            <div className="flex justify-center items-center gap-4">
+                <p className="mt-2 text-blue-400">Home</p>
+                <p className="mt-2 text-blue-400">Jobs</p>
+                <button className="btn btn-dark">Post a Job</button>
+            </div>
+        </div>
+    </div>
+  );
 };
 
 export default Header;
